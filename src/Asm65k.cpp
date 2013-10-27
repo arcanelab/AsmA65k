2169041251d020e730703c6629fa81cf399f82a1
Zoltan Majoros <zoltan@arcanelab.com>
2013-10-27 19:46:58 +0100

    Fixed I_PSH in initializeOpcodetable()

--- src/Asm65k.cpp
+++ src/Asm65k.cpp
@@ -79,12 +79,14 @@ void AsmA65k::initializeOpcodetable()
 
     oa.addressingModesAllowed = { AM_REGISTER1, AM_REGISTER_INDIRECT1, AM_ABSOLUTE1, AM_INDEXED1 };
     oa.instructionCode = I_CLR; opcodes["clr"] = oa;
-    oa.instructionCode = I_PSH; opcodes["psh"] = oa;
     oa.instructionCode = I_POP; opcodes["pop"] = oa;
     oa.instructionCode = I_INC; opcodes["inc"] = oa;
     oa.instructionCode = I_DEC; opcodes["dec"] = oa;
 
-    oa.addressingModesAllowed = { AM_IMMEDIATE,             // Rx, const
+    oa.addressingModesAllowed = { AM_CONST_IMMEDIATE, AM_REGISTER1, AM_REGISTER_INDIRECT1, AM_ABSOLUTE1, AM_INDEXED1 };
+    oa.instructionCode = I_PSH; opcodes["psh"] = oa;
+
+    oa.addressingModesAllowed = { AM_REG_IMMEDIATE,             // Rx, const
                                   AM_REGISTER2,             // Rx, Ry
                                   AM_ABSOLUTE_SRC,          // Rx, [$1234]
                                   AM_ABSOLUTE_DEST,         // [$1234], Rx
