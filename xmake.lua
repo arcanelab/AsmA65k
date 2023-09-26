be663faf5644beab91d9052edf7390974c417a29
Zoltan Majoros <zoltan@arcanelab.com>
2023-09-26 03:40:06 +0200

    Updated standalone and library targets

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,123 @@
 set_project("AsmA65k")
-set_version("0.1.0")
+set_version("0.2.0")
 set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
+set_warnings("all")
 
 if is_mode("debug") then
+    add_defines("DEBUG")
     set_symbols("debug")
     set_optimize("none")
-else
+else 
     set_symbols("hidden")
     set_optimize("fastest")
 end
 
-target("AsmA65k")
-    set_kind("binary")
-    -- set_targetdir("lib")
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
+if is_plat("windows") then
+    set_warnings("all")
+    add_defines("WIN32")
+    add_cxflags("/wd4068")
+    -- add_ldflags("-subsystem:windows")
+elseif is_plat("linux") or is_plat("macosx") or is_plat("mingw") then
+    add_defines("UNIX_HOST")
+    -- add_cxflags("-Wall")
+end
+
+Target =
+{
+    standalone = 1,
+    library = 2
+}
+
+local _target = Target.library
+
+function AddCommon()
     add_includedirs("src")
-    set_targetdir("$(projectdir)")
+    add_files("src/Asm65k.cpp")
+    add_files("src/AsmA65k-Assembly.cpp")
+    add_files("src/AsmA65k-Directives.cpp")
+    add_files("src/AsmA65k-Misc.cpp")
+    set_targetdir("bin")
+end
+
+if _target == Target.standalone then
+    target("AsmA65k")
+        AddCommon()
+        add_files("src/standalone/main.cpp")
+        set_kind("binary")
+elseif _target == Target.library then
+    target("AsmA65k-lib")
+        AddCommon()
+        set_kind("static")
+end
+
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro definition
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
+
